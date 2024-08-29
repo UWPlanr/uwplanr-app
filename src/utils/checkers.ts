96221a4870d4ab922a864b21c2bd43d8ce2e5785
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-29 12:05:03 -0400

    Documenting each prereq condition and moving RegEx expressions to the conditional statements

--- src/utils/checkers.ts
+++ src/utils/checkers.ts
@@ -5,11 +5,6 @@ const gradeRegex = new RegExp("{[0-9]{2,}%}");
 const termRegex = new RegExp("\\[(F|W|S)[0-9]{2}\\]");
 
 const codeFullRegex = new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]?$");
-const codeLevelFullRegex = new RegExp("^[A-Z]{2,} [0-9]XX$");
-const codeLevelPlusFullRegex = new RegExp("[A-Z]{2,} [0-9]XX\\+");
-const codeAllLevelsFullRegex = new RegExp("^[A-Z]{2,} XXX$");
-const codeTermFullRegex = new RegExp("[A-Z]{2,} [0-9]{3}[A-Z]? \\[(F|W|S)[0-9]{2}\\]");
-const codeGradeFullRegex = new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]? {[0-9]{2,}%}$");
 
 const emptyObject = (object: Record<any, any>): boolean => Object.keys(object).length === 0;
 const OR = (values: boolean[]): boolean => values.some(value => value === true);
@@ -29,9 +24,11 @@ const prereqCheckerHelper = (requirement: Requirement, previousCourses: GradeCou
     const booleanValues = [];
     for (let operand of operands) {
         if (typeof operand === "string") {
+            // ABC 123
             if (codeFullRegex.test(operand)) {
                 booleanValues.push((previousCourses.map(previousCourse => previousCourse.code).includes(operand)));
-            } else if (codeGradeFullRegex.test(operand)) {
+            // ABC 123 {XY%}
+            } else if ((new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]? {[0-9]{2,}%}$")).test(operand)) {
                 // @ts-ignore
                 const [code, grade] = [operand.match(codeRegex)[0], parseInt(operand.match(gradeRegex)[0].substring(1, 3))];
                 let found = false;
@@ -42,12 +39,15 @@ const prereqCheckerHelper = (requirement: Requirement, previousCourses: GradeCou
                     };
                 };
                 booleanValues.push(found);
-            } else if (codeAllLevelsFullRegex.test(operand)) {
+            // ABC XXX
+            } else if ((new RegExp("^[A-Z]{2,} XXX$")).test(operand)) {
                 booleanValues.push((previousCourses.map(course => course.code.split(" ")[0])).includes(operand.split(" ")[0]));
-            } else if (codeLevelFullRegex.test(operand)) {
+            // ABC 1XX
+            } else if ((new RegExp("^[A-Z]{2,} [0-9]XX$")).test(operand)) {
                 // @ts-ignore
                 booleanValues.push((previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0])).includes(operand.match("[A-Z]{2,} [0-9]")[0]));
-            } else if (codeLevelPlusFullRegex.test(operand)) {
+            // ABC 1XX+
+            } else if ((new RegExp("[A-Z]{2,} [0-9]XX\\+")).test(operand)) {
                 // @ts-ignore
                 const [operandShortCode, operandNumber] = [operand.match("[A-Z]{2,}")[0], operand.match("[0-9]")[0]];
                 // @ts-ignore
@@ -82,7 +82,7 @@ const antireqChecker = (profile: Term[], course: GradeCourse, index: number): bo
             } else {
                 continue;
             };
-        } else if (codeTermFullRegex.test(antireq)) {
+        } else if ((new RegExp("[A-Z]{2,} [0-9]{3}[A-Z]? \\[(F|W|S)[0-9]{2}\\]")).test(antireq)) {
             // @ts-ignore
             const [code, term] = [antireq.match(codeRegex)[0], antireq.match(termRegex)[0].substring(1, 4)];
             const sameSeasonYearTerms = previousTerms(profile, index).filter(profileTerm => profileTerm.season[0] == term[0] && profileTerm.year.substring(2, 4) == term.substring(1, 3));
