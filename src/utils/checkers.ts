ea44d6e2b103ac7e447a5fdda5b2b6af38e88b76
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-29 18:27:49 -0400

    Changing prereq parser function to 2 separate functions for & and | operations - will generalize in future

--- src/utils/checkers.ts
+++ src/utils/checkers.ts
@@ -1,5 +1,3 @@
-type OperatorFunctionType = (values: boolean[]) => boolean;
-
 const codeRegex = new RegExp("[A-Z]{2,} [0-9]{3}[A-Z]?");
 const gradeRegex = new RegExp("{[0-9]{2,}%}");
 const termRegex = new RegExp("\\[(F|W|S)[0-9]{2}\\]");
@@ -7,9 +5,8 @@ const termRegex = new RegExp("\\[(F|W|S)[0-9]{2}\\]");
 const codeFullRegex = new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]?$");
 
 const emptyObject = (object: Record<any, any>): boolean => Object.keys(object).length === 0;
-const OR = (values: boolean[]): boolean => values.some(value => value === true);
-const AND = (values: boolean[]): boolean => values.every(value => value === true);
-const operatorToFunction = (operator: string): OperatorFunctionType => operator === "&" ? AND : OR;
+const OR = (value1: boolean, value2: boolean): boolean => value1 || value2;
+const operatorToFunction = (operator: string) => operator === "&" ? prereqCheckerAND : prereqCheckerOR;
 
 const previousCourses = (profile: Term[], index: number): GradeCourse[] => {
     return profile.slice(0, index).map(term => term.courses).reduce((initial, current) => initial.concat(current), []);
@@ -19,14 +16,14 @@ const previousTerms = (profile: Term[], index: number): Term[] => {
     return profile.slice(0, index);
 };
 
-const prereqCheckerHelper = (requirement: Requirement, previousCourses: GradeCourse[], operatorFunction: OperatorFunctionType): boolean => {
+const prereqCheckerAND = (requirement: Requirement, previousCourses: GradeCourse[]): boolean => {
     const { operands } = requirement;
-    const booleanValues = [];
+    let final: boolean = true;
     for (let operand of operands) {
         if (typeof operand === "string") {
             // ABC 123
             if (codeFullRegex.test(operand)) {
-                booleanValues.push((previousCourses.map(previousCourse => previousCourse.code).includes(operand)));
+                final = final && previousCourses.map(previousCourse => previousCourse.code).includes(operand);
             // ABC 123 {XY%}
             } else if ((new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]? {[0-9]{2,}%}$")).test(operand)) {
                 // @ts-ignore
@@ -38,37 +35,84 @@ const prereqCheckerHelper = (requirement: Requirement, previousCourses: GradeCou
                         break;
                     };
                 };
-                booleanValues.push(found);
+                final = final && found;
             // ABC XXX
             } else if ((new RegExp("^[A-Z]{2,} XXX$")).test(operand)) {
-                booleanValues.push((previousCourses.map(course => course.code.split(" ")[0])).includes(operand.split(" ")[0]));
+                final = final && previousCourses.map(course => course.code.split(" ")[0]).includes(operand.split(" ")[0]);
             // ABC 1XX
             } else if ((new RegExp("^[A-Z]{2,} [0-9]XX$")).test(operand)) {
                 // @ts-ignore
-                booleanValues.push((previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0])).includes(operand.match("[A-Z]{2,} [0-9]")[0]));
+                final = final && previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0]).includes(operand.match("[A-Z]{2,} [0-9]")[0]);
             // ABC 1XX+
             } else if ((new RegExp("[A-Z]{2,} [0-9]XX\\+")).test(operand)) {
                 // @ts-ignore
                 const [operandShortCode, operandNumber] = [operand.match("[A-Z]{2,}")[0], operand.match("[0-9]")[0]];
                 // @ts-ignore
-                booleanValues.push(OR((previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0])).map(shortCode => {
+                final = final && OR((previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0])).map(shortCode => {
                     // @ts-ignore
                     const [subjectCode, number] = [shortCode.match("[A-Z]{2,}")[0], shortCode.match("[0-9]")[0]];
                     return subjectCode === operandShortCode && parseInt(number) >= parseInt(operandNumber);
-                })));
+                }));
             };
         } else {
-            booleanValues.push(prereqCheckerHelper(operand, previousCourses, operatorToFunction(operand.operator)));
+            final = final && operatorToFunction(operand.operator)(operand, previousCourses);
         };
+        if (!final) return false;
     };
-    return operatorFunction(booleanValues);
+    return final;
+};
+
+const prereqCheckerOR = (requirement: Requirement, previousCourses: GradeCourse[]): boolean => {
+    const { operands } = requirement;
+    let final: boolean = false;
+    for (let operand of operands) {
+        if (typeof operand === "string") {
+            // ABC 123
+            if (codeFullRegex.test(operand)) {
+                final = final || previousCourses.map(previousCourse => previousCourse.code).includes(operand);
+            // ABC 123 {XY%}
+            } else if ((new RegExp("^[A-Z]{2,} [0-9]{3}[A-Z]? {[0-9]{2,}%}$")).test(operand)) {
+                // @ts-ignore
+                const [code, grade] = [operand.match(codeRegex)[0], parseInt(operand.match(gradeRegex)[0].substring(1, 3))];
+                let found = false;
+                for (let previousCourse of previousCourses) {
+                    if (previousCourse.code === code && parseInt(previousCourse.grade) >= grade) {
+                        found = true;
+                        break;
+                    };
+                };
+                final = final || found;
+            // ABC XXX
+            } else if ((new RegExp("^[A-Z]{2,} XXX$")).test(operand)) {
+                final = final || previousCourses.map(course => course.code.split(" ")[0]).includes(operand.split(" ")[0]);
+            // ABC 1XX
+            } else if ((new RegExp("^[A-Z]{2,} [0-9]XX$")).test(operand)) {
+                // @ts-ignore
+                final = final || previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0]).includes(operand.match("[A-Z]{2,} [0-9]")[0]);
+            // ABC 1XX+
+            } else if ((new RegExp("[A-Z]{2,} [0-9]XX\\+")).test(operand)) {
+                // @ts-ignore
+                const [operandShortCode, operandNumber] = [operand.match("[A-Z]{2,}")[0], operand.match("[0-9]")[0]];
+                // @ts-ignore
+                final = final || OR((previousCourses.map(course => course.code.match("[A-Z]{2,} [0-9]")[0])).map(shortCode => {
+                    // @ts-ignore
+                    const [subjectCode, number] = [shortCode.match("[A-Z]{2,}")[0], shortCode.match("[0-9]")[0]];
+                    return subjectCode === operandShortCode && parseInt(number) >= parseInt(operandNumber);
+                }));
+            };
+        } else {
+            final = final || operatorToFunction(operand.operator)(operand, previousCourses);
+        };
+        if (final) return true;
+    };
+    return final;
 };
 
 const prereqChecker = (profile: Term[], course: GradeCourse, index: number): boolean => {
     if (emptyObject(JSON.parse(course.prereqs))) return true;
     const courses = previousCourses(profile, index);
     const prereqs = JSON.parse(course.prereqs) as Requirement;
-    return prereqCheckerHelper(prereqs, courses, operatorToFunction(prereqs.operator))
+    return operatorToFunction(prereqs.operator)(prereqs, courses);
 };
 
 const antireqChecker = (profile: Term[], course: GradeCourse, index: number): boolean => {
