1679919e033ef43add23644de0d1920569188cae
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-30 21:13:41 -0400

    Updating information modal with colors - TBA a central color tagging system

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -7,14 +7,38 @@ const InfoModal = () => {
         <dialog id="info-modal" className="modal">
         <div className="modal-box">
             <h3 className="font-bold text-lg">Information</h3>
-            <div className="flex flex-wrap justify-center items-center gap-4 mt-4">
-                <div className="flex items-center gap-x-2">
-                    <span className="btn btn-xs btn-circle bg-green-500 hover:bg-green-500"></span>
-                    <span>All requirements met</span>
-                </div>
-                <div className="flex items-center gap-x-2">
-                    <span className="btn btn-xs btn-circle bg-red-500 hover:bg-red-500"></span>
-                    <span>Prerequisites not met</span>
+            <div className="mt-4">
+                <div className="overflow-x-auto">
+                    <table className="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Status</th>
+                                <th>Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>All course requirements met</td>
+                                <td><span className="btn btn-xs btn-circle bg-green-500 hover:bg-green-500"></span></td>
+                            </tr>
+                            <tr>
+                                <td>Course prerequisites not met</td>
+                                <td><span className="btn btn-xs btn-circle bg-red-500 hover:bg-red-500"></span></td>
+                            </tr>
+                            <tr>
+                                <td>Course antirequisites not met</td>
+                                <td><span className="btn btn-xs btn-circle bg-orange-500 hover:bg-orange-500"></span></td>
+                            </tr>
+                            <tr>
+                                <td>Course minimum level not met</td>
+                                <td><span className="btn btn-xs btn-circle bg-indigo-500 hover:bg-indigo-500"></span></td>
+                            </tr>
+                            <tr>
+                                <td>Course can not be taken in the term</td>
+                                <td><span className="btn btn-xs btn-circle bg-cyan-500 hover:bg-cyan-500"></span></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div className="modal-action">
